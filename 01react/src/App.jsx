e010422edc86e4842f0a9be71bbc8d96bfdf6f25
Sahil <leviack629@gmail.com>
2023-09-28 11:07:15 +0530

    workflow

--- 01react/src/App.jsx
+++ 01react/src/App.jsx
@@ -1,14 +1,13 @@
-
+import Chai from "./Chai";
 
 
 function App() {
-
-
   return (
     <>
       <h1>Vite + React</h1>
+      <Chai />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
