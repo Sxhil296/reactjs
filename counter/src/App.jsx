51b9c4c94380c954bc8adfbcd20a6b79cf166087
Sahil <leviack629@gmail.com>
2023-09-28 22:11:03 +0530

    counter app - hooks

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,22 +1,30 @@
 
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-
-  //vanilla javascript
-let counter = 6
+//vanilla javascript
+// let counter = 6
+// const increaseValue = () => {
+//   console.log('increase', counter)
+//   counter = counter+1
+// }
+// const decreaseValue = () => {
+//   console.log('increase', Math.random())
+// }
+const [count, setCount] = useState(0)
 const increaseValue = () => {
-  console.log('increase', counter)
-  counter = counter+1
+  setCount(count +1)
 }
 const decreaseValue = () => {
-  console.log('increase', Math.random())
+  setCount(count -1)
 }
+
 
   return (
     <>
       <h1>Counter</h1>
-      <h2>Counter Value : {counter}</h2>
+      <h2>Counter Value : {count}</h2>
       <button onClick={increaseValue}>Increase</button>
       <button onClick={decreaseValue}>Decrease</button>
     </>
