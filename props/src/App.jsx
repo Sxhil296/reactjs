5eb5f48470b2f2f2a14d57e5a8b2ceed64f310b8
Sahil <leviack629@gmail.com>
2023-09-29 14:54:31 +0530

    tailwindcss and props

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,13 +1,19 @@
-
-import './App.css'
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-
+  let myObj = {
+    name: "Patrick",
+    age: "28",
+  };
+  let myArr = [1, 2, 3];
   return (
     <>
-     <h1 className='bg-green-800 rounded font-bold p-4'>Tailwind Test</h1>
+      <h1 className="bg-green-800 rounded font-bold p-4 mb-4">Tailwind Test</h1>
+      <Card username="Batman" someObj={myObj} newArr={myArr} />
+      <Card username="Catwoman"/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
